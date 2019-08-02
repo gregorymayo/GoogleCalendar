dc86330ef2c9ade1c89b02973d18606fce3848cc
Gregory Jueves Mayo <gregoryjuevesii23@gmail.com>
2019-08-01 22:48:39 -0700

    Update SimpleCalendar.java

--- src/SimpleCalendar.java
+++ src/SimpleCalendar.java
@@ -1,3 +1,8 @@
+/**
+ * SimpleCalendar.java
+ * @author Tyler Lorenzi, Gregory Mayo, Ealrada Piroyan
+ * @version 1.0 08/02/19
+ */
 public class SimpleCalendar {
 	public static void main(String[] args) {
 		CalendarModel cm = new CalendarModel();
